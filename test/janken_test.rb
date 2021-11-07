01caa2340c342800193733381c4dfa51221ebc34
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-07 15:40:46 +0900

    テスト内の重複の除去

--- test/janken_test.rb
+++ test/janken_test.rb
@@ -7,42 +7,40 @@ class JankenTest < Minitest::Test
   # 0: グー, 1: チョキ, 2: パー
 
   class JapaneseJanken < self
-    def test_win
+    def setup
       display = JapaneseDisplay.new
-      janken = Janken.new(display)
-      assert_equal('勝利', janken.play(0, 1))
+      @janken = Janken.new(display)
+    end
+
+    def test_win
+      assert_equal('勝利', @janken.play(0, 1))
     end
 
     def test_draw
-      display = JapaneseDisplay.new
-      janken = Janken.new(display)
-      assert_equal('引き分け', janken.play(0, 0))
+      assert_equal('引き分け', @janken.play(0, 0))
     end
 
     def test_lose
-      display = JapaneseDisplay.new
-      janken = Janken.new(display)
-      assert_equal('敗北', janken.play(0, 2))
+      assert_equal('敗北', @janken.play(0, 2))
     end
   end
 
   class EnglishJanken < self
-    def test_win
+    def setup
       display = EnglishDisplay.new
-      janken = Janken.new(display)
-      assert_equal('Win', janken.play(0, 1))
+      @janken = Janken.new(display)
+    end
+
+    def test_win
+      assert_equal('Win', @janken.play(0, 1))
     end
 
     def test_draw
-      display = EnglishDisplay.new
-      janken = Janken.new(display)
-      assert_equal('Draw', janken.play(1, 1))
+      assert_equal('Draw', @janken.play(1, 1))
     end
 
     def test_lose
-      display = EnglishDisplay.new
-      janken = Janken.new(display)
-      assert_equal('Lose', janken.play(0, 2))
+      assert_equal('Lose', @janken.play(0, 2))
     end
   end
 end
