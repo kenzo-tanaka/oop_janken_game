5b9608d21098b5ff21f16e46ac3bedc10879ce15
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-07 13:10:31 +0900

    じゃんけんの判定をメソッドに分離
    https://speakerdeck.com/nrslib/introduction-to-oop-with-php?slide=70

--- lib/janken.rb
+++ lib/janken.rb
@@ -1,8 +1,46 @@
 class Janken
   def play(left, right)
-    return '引き分け' if left == right
-    return '敗北' if left == 0 && right == 2
+    result = judge(left, right)
+    show_result(result)
+  end
 
-    '勝利'
+  private
+
+  def judge(left, right)
+    if left == 0
+      if right == 0
+        return 0
+      elsif right == 1
+        return 1
+      else
+        return -1
+      end
+    elsif left == 1
+      if right == 0
+        return -1
+      elsif right ==1
+        return 0
+      else
+        return 1
+      end
+    else
+      if right == 2
+        return 1
+      elsif right == 1
+        return -1
+      else
+        return 0
+      end
+    end
+  end
+
+  def show_result(result)
+    if result == 1
+      '勝利'
+    elsif result == 0
+      '引き分け'
+    else
+      '敗北'
+    end
   end
 end
