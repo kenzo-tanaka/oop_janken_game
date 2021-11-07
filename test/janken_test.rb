7fbefe455ba552d0f026b7b5e0dec5835864f666
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-07 15:10:41 +0900

    テストに階層構造を追加
    これから多国籍対応するため

--- test/janken_test.rb
+++ test/janken_test.rb
@@ -3,18 +3,21 @@ require_relative '../lib/janken'
 
 class JankenTest < Minitest::Test
   # 0: グー, 1: チョキ, 2: パー
-  def test_win_ja
-    janken = Janken.new
-    assert_equal('勝利', janken.play(0, 1, 'ja'))
-  end
 
-  def test_draw_ja
-    janken = Janken.new
-    assert_equal('引き分け', janken.play(0, 0, 'ja'))
-  end
+  class JapaneseTest < self
+    def test_win_ja
+      janken = Janken.new
+      assert_equal('勝利', janken.play(0, 1, 'ja'))
+    end
 
-  def test_lose_ja
-    janken = Janken.new
-    assert_equal('敗北', janken.play(0, 2, 'ja'))
+    def test_draw_ja
+      janken = Janken.new
+      assert_equal('引き分け', janken.play(0, 0, 'ja'))
+    end
+
+    def test_lose_ja
+      janken = Janken.new
+      assert_equal('敗北', janken.play(0, 2, 'ja'))
+    end
   end
 end
