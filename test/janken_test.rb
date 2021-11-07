879b972848f8c324ac3a81b2cf4527859d770f2e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-11-07 13:01:42 +0900

    refactor: test_play -> tet_winに変更
    変数もインライン化

--- test/janken_test.rb
+++ test/janken_test.rb
@@ -3,9 +3,8 @@ require_relative '../lib/janken'
 
 class JankenTest < Minitest::Test
   # 0: グー, 1: チョキ, 2: パー
-  def test_play
+  def test_win
     janken = Janken.new
-    actual = janken.play(0, 1)
-    assert_equal('勝利', actual)
+    assert_equal('勝利', janken.play(0, 1))
   end
 end
